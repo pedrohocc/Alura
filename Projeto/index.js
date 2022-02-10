e3bc3795db39a697a65652478c0ab1389e09490e
Pedro Henrique Coriolano Oliveira Cruz <93086323+pedrohcoriolano@users.noreply.github.com>
2022-02-10 19:08:38 -0300

    att

--- Projeto/index.js
+++ Projeto/index.js
@@ -1,15 +1,12 @@
 import {Cliente} from "./Cliente.js";
 import { ContaCorrente } from "./ContaCorrente.js";
 import { ContaPoupanca } from "./ContaPoupanca.js";
+import { Conta } from "./Conta.js";
 
 const cliente1 = new Cliente("Ricardo", 11122233390);
 
 const ContaCorrente1 = new ContaCorrente(cliente1, 1001);
-ContaCorrente1.depositar(500);
-ContaCorrente1.sacar(100);
-
 const contaPoupanca = new ContaPoupanca(50, cliente1, 1002);
-contaPoupanca.sacar(10);
+const conta = new Conta(0, cliente1, 1003);
 
-console.log(ContaCorrente1);
-console.log(contaPoupanca);
+console.log(conta);
