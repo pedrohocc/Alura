53a68bc6036c7a2ee9b86dd61effc718b41043fc
Pedro Henrique Coriolano Oliveira Cruz <93086323+pedrohcoriolano@users.noreply.github.com>
2022-02-11 23:27:25 -0300

    correção de bug(pc)

--- Projeto/funcionarios/Funcionario.js
+++ Projeto/funcionarios/Funcionario.js
@@ -8,7 +8,7 @@ export class Funcionario {
     }
 
     autenticar(senha){
-        return senha = this._senha;
+        return senha == this._senha;
     }
 
     cadastrarSenha(senha){
--- Projeto/index.js
+++ Projeto/index.js
@@ -5,14 +5,13 @@ import { SistemaAutenticacao } from "./SistemaAutenticacao.js";
 
 const diretor = new Diretor("Pedro", 20000, 12345678900);
 diretor.cadastrarSenha("senha123");
+const diretorLogado = SistemaAutenticacao.login(diretor, "senha123");
 
 const gerente = new Gerente("Paulo", 9500, 22233344419);
 gerente.cadastrarSenha("senha456");
+const gerenteLogado = SistemaAutenticacao.login(gerente, "senha456");
 
 const mylena = new Cliente("Mylena", 2344349343, 1978);
-
-const diretorLogado = SistemaAutenticacao.login(diretor, "senha123");
-const gerenteLogado = SistemaAutenticacao.login(gerente, "senha456");
 const mylenaLogado = SistemaAutenticacao.login(mylena, "1978");
 
 console.log(mylenaLogado, gerenteLogado, diretorLogado);
