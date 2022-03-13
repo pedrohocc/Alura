5d37919e85ffeb6ab0126e6da27a49e648114f8a
Pedro Henrique Coriolano Oliveira Cruz <93086323+pedrohcoriolano@users.noreply.github.com>
2022-03-13 16:57:54 -0300

    fim do curso

--- JavaScript-Web/main.js
+++ JavaScript-Web/main.js
@@ -1,11 +1,43 @@
-function toca_som_pom() {
-    document.querySelector('#som_tecla_pom').play();
+function toca_som(seletorAudio) {
+
+    const elemento = document.querySelector(seletorAudio);
+    
+    if (elemento && elemento.localName === 'audio') {
+        
+        elemento.play()
+        
+    } else {
+        console.error('Elemento não encontrado ou Seletor inválido')
+    }
+    
 }
 
-function toca_som_clap() {
-    document.querySelector('#som_tecla_clap').play();
+const lista_teclas = document.querySelectorAll('.tecla');
+
+for (let contador = 0; contador < lista_teclas.length; contador++) {
+
+    const tecla = lista_teclas[contador]
+    const nome = tecla.classList[1];
+    const idAudio = `#som_${nome}`;
+
+    tecla.onclick = function () {
+
+        toca_som(idAudio);
+
+    }
+
+    tecla.onkeydown = function(evento_tecla) {
+
+        let tecla_apertada = evento_tecla.code;
+
+        tecla_apertada === 'Enter' || tecla_apertada === 'Space' ? tecla.classList.add('ativa') : console.log(false);
+
+    }
+
+    tecla.onkeyup = function() {
+        
+        tecla.classList.remove('ativa');
+        
+    }
+
 }
-
-document.querySelector('.tecla_pom').onclick = toca_som_pom;
-
-document.querySelector('.tecla_clap').onclick = toca_som_clap;
