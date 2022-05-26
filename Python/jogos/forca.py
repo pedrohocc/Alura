83522f1065346ed55dc29df13dc21bde1f655400
Pedro Henrique Coriolano Oliveira Cruz <93086323+pedrohcoriolano@users.noreply.github.com>
2022-05-26 10:21:26 -0300

    att

--- Python/jogos/forca.py
+++ Python/jogos/forca.py
@@ -2,7 +2,23 @@ def jogar():
     print('*'*50)
     print('Bem vindo ao jogo da forca!')
     print('*'*50)
-
+    
+    palavra_secreta = 'banana'
+    
+    enforcou = False
+    
+    acertou = False
+    
+    index = 0
+    
+    while(not enforcou and not acertou ):
+        letra = input('Digite uma letra? ').strip().lower()
+        for contador in palavra_secreta:
+            if(letra == contador):
+                print('Encotrei a letra "{}" na posição {}'.format(contador.upper(), index))
+            index += 1
+        print('jogando...')
+                
     print('Fim de jogo.')
 if (__name__ == '__main__'):
     jogar()
