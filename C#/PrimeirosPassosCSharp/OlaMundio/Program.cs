d45a43ddcfec23838edb0aee3412c5699e99a5ad
Pedro Henrique Coriolano Oliveira Cruz <93086323+pedrohcoriolano@users.noreply.github.com>
2022-07-07 22:25:37 -0300

    CSharp

---
+++ C#/PrimeirosPassosCSharp/OlaMundio/Program.cs
@@ -0,0 +1,10 @@
+﻿using System;
+class Pograma
+{
+    static void Main(string[] arg)
+    {
+        Console.WriteLine("Olá, mundo");
+        Console.WriteLine("tecle enter para fechar");
+        Console.ReadLine();
+    }
+}
