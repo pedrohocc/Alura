d99a0855acea207acbf853ead34effab4bf8e7bb
Pedro Henrique Coriolano Oliveira Cruz <93086323+pedrohcoriolano@users.noreply.github.com>
2022-07-10 00:05:31 -0300

    novos ex

---
+++ CSharp Orientado a Objeto/ByteBank/ContaCorrente.cs
@@ -0,0 +1,71 @@
+﻿namespace ByteBank
+{
+    public class ContaCorrente
+    {
+        public string titular;
+        public string conta;
+        public int NumeroAgencia;
+        public string NomeAgencia;
+        public double saldo;
+
+        public bool Sacar(double valor)
+        {
+            if(valor > saldo)
+            {
+                return false;
+            }
+            else if (valor < 0)
+            {
+                return false;
+            }
+            else if (valor <= saldo) 
+            {
+                saldo = saldo - valor;
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public void Depositar(double valor)
+        {
+            if (valor > 0)
+            {
+                saldo = saldo + valor;
+            }
+        }
+
+        public bool Transferir(double valor, ContaCorrente destino)
+        {
+            if (valor > saldo)
+            {
+                return false;
+            }
+            else if (valor < 0)
+            {
+                return false;
+            }
+            else if (valor <= saldo)
+            {
+                saldo = saldo - valor;
+                destino.saldo = destino.saldo + valor;
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public void Informacoes()
+        {
+            Console.WriteLine(titular);
+            Console.WriteLine(conta);
+            Console.WriteLine(NumeroAgencia);
+            Console.WriteLine(NomeAgencia);
+            Console.WriteLine(saldo);
+        }
+    }
+}
---
+++ CSharp Orientado a Objeto/ByteBank/Program.cs
@@ -0,0 +1,21 @@
+﻿using ByteBank;
+
+Console.WriteLine("Bem vindo ao ByteBank, seu banco do futuro!");
+
+ContaCorrente Conta1 = new ContaCorrente();
+Conta1.titular = "Andressa Bessa";
+Conta1.conta = "12345-x";
+Conta1.NumeroAgencia = 12345;
+Conta1.NomeAgencia = "Central";
+Conta1.saldo = 90.55;
+
+ContaCorrente Conta2 = new ContaCorrente();
+Conta2.titular = "Andressa Besta";
+Conta2.conta = "12345-x";
+Conta2.NumeroAgencia = 12345;
+Conta2.NomeAgencia = "Central";
+Conta2.saldo = 100;
+
+Console.WriteLine(Conta2.Informacoes);
+
+Console.ReadKey();
