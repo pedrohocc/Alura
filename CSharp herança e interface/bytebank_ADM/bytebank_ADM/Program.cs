b3eb1d2936170be823ba6aa7b9aad5785967369a
Pedro Henrique Coriolano Oliveira Cruz <93086323+pedrohcoriolano@users.noreply.github.com>
2022-07-13 13:18:19 -0300

    Curso de herança e interface

---
+++ CSharp herança e interface/bytebank_ADM/bytebank_ADM/Program.cs
@@ -0,0 +1,7 @@
+﻿using bytebank_ADM.funcionarios;
+
+Console.WriteLine("Bem vindo ao ByteBank Administrativo!");
+
+Funcionario pedro = new("Pedro Cruz", "000.000.000-31", 2500);
+
+Console.WriteLine($"A Bonificação do Funcionário {pedro.Nome} cujo o CPF é {pedro.Cpf} é igual a R$ R$ {pedro.Bonificacao()}");
---
+++ CSharp herança e interface/bytebank_ADM/bytebank_ADM/funcionarios/Funcionario.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bytebank_ADM.funcionarios
+{
+    public class Funcionario
+    {
+        private string _nome;
+        private string _cpf;
+        private double _salario;
+
+        public string Nome { get => _nome; set => _nome = value; }
+        public string Cpf { get => _cpf; set => _cpf = value; }
+        public double Salario { get => _salario; set => _salario = value; }
+
+        public double Bonificacao()
+        {
+            return Salario * 0.1;
+        }
+
+        public Funcionario(string nome, string cpf, double salario)
+        {
+            Nome = nome;
+            Cpf = cpf;
+            Salario = salario;
+        }
+    }
+}
