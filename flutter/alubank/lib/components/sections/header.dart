a712f0c54d42e87cf1d987848780837e8e84258d
Pedro Henrique <93086323+pedrohocc@users.noreply.github.com>
2023-07-11 23:21:25 -0300

    modulo `Começando o projeto`

---
+++ flutter/alubank/lib/components/sections/header.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class Header extends StatelessWidget {
+  const Header({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Row(children: [
+      Column(
+        children: [Text('\$1000.00'), Text('Balanço disponível')],
+      ),
+      Icon(Icons.account_circle)
+    ]);
+  }
+}
