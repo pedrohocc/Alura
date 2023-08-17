6dca8d0e5ec79bb046092b65ea10741e1ce4c952
Pedro Henrique <93086323+pedrohocc@users.noreply.github.com>
2023-08-16 21:14:14 -0300

    fim curso

--- spring/api/src/main/java/med/voll/api/models/medico/Medico.java
+++ spring/api/src/main/java/med/voll/api/models/medico/Medico.java
@@ -38,6 +38,8 @@ public class Medico {
     @Embedded
     private Endereco endereco;
 
+    private Boolean ativo;
+
     public Medico(DadosCadastroMedico dados) {
         this.nome = dados.nome();
         this.email = dados.email();
@@ -45,5 +47,24 @@ public class Medico {
         this.telefone = dados.telefone();
         this.especialidade = dados.especialidade();
         this.endereco = new Endereco(dados.endereco());
+        this.ativo = true;
+    }
+
+    public void atualizarInformacoes(DadosAtualizacaoMedico dados) {
+        if(dados.nome() != null) {
+            this.nome = dados.nome();
+        }
+
+        if(dados.telefone() != null) {
+            this.telefone = dados.telefone();
+        }
+
+        if(dados.endereco() != null) {
+            this.endereco.atualizarInformacoes(dados.endereco());
+        }
+    }
+
+    public void desativar() {
+        this.ativo = false;
     }
 }
