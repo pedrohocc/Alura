1c4a647d6a52f1fa9ee5d657f94161350fb54815
Pedro Henrique <93086323+pedrohocc@users.noreply.github.com>
2023-11-07 19:48:42 -0300

    novas aulas

--- react/organo/src/App.js
+++ react/organo/src/App.js
@@ -2,6 +2,7 @@ import { useState } from 'react';
 import './App.css'
 import Banner from "./componentes/Banner/Index.js";
 import Formulario from "./componentes/Formulario/Index.js";
+import Time from './componentes/Time/index.js';
 
 function App() {
   const [colaboradores, setColaboradores] = useState([])
@@ -14,6 +15,8 @@ function App() {
     <div className="App">
       <Banner />
       <Formulario funcao={novoColaborador => atualizarColaboladores(novoColaborador)} />
+      <Time nome="Programação"></Time>
+      <Time nome="Front-End"></Time>
     </div>
   );
 }
